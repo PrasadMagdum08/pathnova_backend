dd7868d34748f2aa1978aace6c8d268b5fb3deb3
Prasad Magdum <magdumprasad194@gmail.com>
2025-07-09 23:08:02 +0530

    update changes

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+  role: { type: String, enum: ['student', 'admin'], default: 'student' }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
