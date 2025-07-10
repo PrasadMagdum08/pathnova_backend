4882a1cb590626216779bfb69b6302dbc18082fa
Prasad Magdum <magdumprasad194@gmail.com>
2025-07-10 13:31:06 +0530

    update changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 exports.register = async (req, res) => {
   const { name, email, password, role } = req.body;
   try {
