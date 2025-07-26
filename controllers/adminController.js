5f131b7e4097aaa2e5e12402d8a46f6116b3afe1
Prasad Magdum <magdumprasad194@gmail.com>
2025-07-26 17:39:45 +0530

    update changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -81,7 +81,7 @@ exports.assignTask = async (req, res) => {
       description,
       assignedToBatch,
       assignedToStudent,
-      assignedBy: req.Admin.id
+      assignedBy: req.Admin.user.id
     });
     res.status(201).json(task);
   } catch (err) {
