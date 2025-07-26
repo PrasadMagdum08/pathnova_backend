a3e1d6379fb8abd528e8e47451b3b9b95c7a93b2
Prasad Magdum <magdumprasad194@gmail.com>
2025-07-26 14:02:34 +0530

    update changes

--- models/Admin.js
+++ models/Admin.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 
 const AdminSchema = new mongoose.Schema({
     name: String,
-    email: {type: String, unique: true},
-    password: String,
-    role: {type: String, default: 'admin'}
-});
+    email: String,
+    whatsapp: Number,
+}, {timestamps: true});
 
 module.exports = mongoose.model('Admin', AdminSchema);
--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
-  password: String,
+  email: { type: String, required: true, unique: true },
+  password: {type: String, required: true},
+  whatsapp_contact: { type: Number, required: true, unique: true },
   role: { type: String, enum: ['student', 'admin'], default: 'student' }
 }, { timestamps: true });
 
